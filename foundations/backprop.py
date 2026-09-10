f73c9618fd227f893cb1fe09110519d56ba9dfee
paolocecco <85558178+paolocecco@users.noreply.github.com>
2026-09-10 10:13:48 +0100

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -17,7 +17,7 @@ class Solution:
         z = np.dot(x, w) + b
         y_hat = 1 / (1 + np.exp(-z))
 
-        loss = (1/2) * (y_hat - y_true)**2
+        loss = 0.5 * np.square(y_hat - y_true)
 
         dL_dw = (y_hat - y_true) * y_hat * (1 - y_hat) * x
         dL_dw = np.round(dL_dw, 5)
